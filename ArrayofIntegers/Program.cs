422128b3120492f4e858ccd837ddf504340ce8f3
Support <Support@WEMA-TRAINING-S>
2022-08-11 21:20:32 +0100

    Add project files.

---
+++ ArrayofIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ArrayofIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[] { 25, 15, 10, 7, 56, 100, 63, 71, };
+            int temp;
+            
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = i +1; j < 8; j++)
+                {
+                    if (num[i] < num[j])
+                    {
+                        temp = num[i];
+                        num[i] = num[j];
+                        num[j] = temp;
+                    }
+                }
+                //Console.WriteLine(string.Join(" ", num));
+                //Console.WriteLine(num)
+            }
+            Console.WriteLine("Array in Descending order: ");
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
